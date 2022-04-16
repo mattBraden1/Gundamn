a95e61acf45342c5849761df2a8653ea39610e0c
matt <mjbraden@umich.edu>
2022-04-16 19:27:06 -0400

    Updated the player shooting script

--- Assets/Scripts/Enemy/EnemyShooting.cs
+++ Assets/Scripts/Enemy/EnemyShooting.cs
@@ -5,30 +5,47 @@ using UnityEngine;
 public class EnemyShooting : MonoBehaviour
 {
     [SerializeField]
+    private float yOffset = -1.5f;
+    [SerializeField]
     public GameObject projectile;
     [SerializeField]
     private float fireRate = 20;
     //private float interval;
     private bool isShooting = true, startCoroutine = false;
-    void Start()
+    [SerializeField]
+    private bool burst = false;
+    [SerializeField]
+    private int burstShots = 1;
+    private int curShots;
+
+    private void Start()
     {
-        
+        curShots = burstShots;
     }
 
     void Shoot()
     {
-        Instantiate(projectile, new Vector3(transform.position.x, transform.position.y - 1.5f, transform.position.z), transform.rotation);
+        Instantiate(projectile, new Vector3(transform.position.x, transform.position.y + yOffset, transform.position.z), transform.rotation);
     }
 
     void Update()
     {
-        if (isShooting)
+        if (isShooting & !burst)
         {
             Shoot();
             isShooting = false;
         }
+        else if (curShots > 0 && isShooting && !startCoroutine)
+        {
+            Shoot();
+            curShots--;
+            StartCoroutine(WaitForTime(.5f));
+        }
         else if (!startCoroutine)
+        {
             StartCoroutine(WaitForTime(fireRate));
+            curShots = burstShots;
+        }
     }
     IEnumerator WaitForTime(float timeToWait)
     {
